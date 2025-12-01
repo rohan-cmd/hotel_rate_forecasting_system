52f5f42ae2ecb7498cef45dbbdbcc07db5ba5253
Rohan <rohanp10240@gmail.com>
2025-12-02 03:41:45 +0530

    Added initial HTML pages and Flask routes for RMS project

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/predict")
+def predict():
+    return render_template("predict.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
