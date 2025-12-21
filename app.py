f6b73633c5f8cad2b09d9cc3f216a0f5b83a9224
Rohan <rohanp10240@gmail.com>
2025-12-21 08:04:35 +0530

    Implement secure login functionality with password hashing and session management

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 from db import get_connection
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = '_system_management_revenue_'
 
 # ------------------------------
 # DATABASE SETUP
@@ -40,11 +41,41 @@ def about():
 
 @app.route("/predict")
 def predict():
+    if "user" not in session:
+        return redirect(url_for("login"))
+
     return render_template("predict.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * from user_registrations WHERE username=?", (username,))
+        row = cursor.fetchone()
+        conn.close()
+
+        if row:
+            columns = [col[0] for col in cursor.description]
+            user = dict(zip(columns, row))
+        else:
+            user = None
+
+        if user and check_password_hash(user["password_hash"], password):
+            session["user"] = username
+            return redirect(url_for("home"))
+        else:
+            return "Invalid Username or Password!"
+
     return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
